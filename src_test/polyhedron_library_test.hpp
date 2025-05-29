b104455e1a6a9b8c4bfc85876c0670eeb5ae6508
enricozangi <s307815@studenti.polito.it>
2025-05-29 12:30:03 +0200

    fixed test fot sortingEdges

--- src_test/polyhedron_library_test.hpp
+++ src_test/polyhedron_library_test.hpp
@@ -72,64 +72,54 @@ TEST(isValidTest, DiscontinuityTest)
 TEST(SortTest, SortEdgesTest)
 {
 
-    const double phi = (1.0 + sqrt(5.0)) / 2.0;
-    const double inv_norm = 1.0 / sqrt(1 + phi * phi);
+    using std::numbers::phi;
 
-    vector<Vertex> vertices_list =
-    {
-        {0, -1 * inv_norm,  phi * inv_norm, 0, false},
-        {1,  1 * inv_norm,  phi * inv_norm, 0, false},
-        {2, -1 * inv_norm, -phi * inv_norm, 0, false},
-        {3,  1 * inv_norm, -phi * inv_norm, 0, false},
-        {4, 0, -1 * inv_norm,  phi * inv_norm, false},
-        {5, 0,  1 * inv_norm,  phi * inv_norm, false},
-        {6, 0, -1 * inv_norm, -phi * inv_norm, false},
-        {7, 0,  1 * inv_norm, -phi * inv_norm, false},
-        {8,  phi * inv_norm, 0, -1 * inv_norm, false},
-        {9,  phi * inv_norm, 0,  1 * inv_norm, false},
-        {10, -phi * inv_norm, 0, -1 * inv_norm, false},
-        {11, -phi * inv_norm, 0,  1 * inv_norm, false}
-    }
-    ;
-
-    vector<Edge> edges_list =
-    {
-        {0, 0, 11, false}, {1, 0, 5, false},  {2, 0, 1, false},  {3, 0, 7, false},  {4, 0, 10, false},
-        {5, 0, 2, false},  {6, 1, 5, false},  {7, 1, 9, false},  {8, 1, 8, false},  {9, 1, 7, false},
-        {10, 1, 0, false}, {11, 2, 10, false},{12, 2, 3, false}, {13, 2, 6, false}, {14, 2, 0, false},
-        {15, 3, 2, false}, {16, 3, 6, false}, {17, 3, 8, false}, {18, 3, 9, false}, {19, 3, 1, false},
-        {20, 4, 10, false},{21, 4, 11, false},{22, 4, 5, false}, {23, 4, 6, false}, {24, 4, 2, false},
-        {25, 5, 11, false},{26, 5, 9, false}, {27, 6, 10, false},{28, 6, 3, false}, {29, 6, 4, false}
+	vector<Vertex> vertices_list = {
+        {0, -1, phi, 0, false},
+        {1, 1, phi, 0, false},
+        {2, -1, -phi, 0, false},
+        {3, 1, -phi, 0, false},
+        {4, 0, -1, phi, false},
+        {5, 0, 1, phi, false},
+        {6, 0, -1, -phi, false},
+        {7, 0, 1, -phi, false},
+        {8, phi, 0, -1, false},
+        {9, phi, 0, 1, false},
+        {10, -phi, 0, -1, false},
+        {11, -phi, 0, 1, false}
     };
 
-
-    vector<Face> faces_list =
-    {
-        {0, {vertices_list[0], vertices_list[11], vertices_list[5]}, {edges_list[0], edges_list[25], edges_list[1]}},
-        {1, {vertices_list[0], vertices_list[5], vertices_list[1]}, {edges_list[1], edges_list[6], edges_list[2]}},
-        {2, {vertices_list[0], vertices_list[1], vertices_list[7]}, {edges_list[2], edges_list[9], edges_list[3]}},
-        {3, {vertices_list[0], vertices_list[7], vertices_list[2]}, {edges_list[3], edges_list[13], edges_list[5]}},
-        {4, {vertices_list[0], vertices_list[2], vertices_list[10]}, {edges_list[5], edges_list[11], edges_list[4]}},
-
-        {5, {vertices_list[1], vertices_list[5], vertices_list[9]}, {edges_list[6], edges_list[26], edges_list[7]}},
-        {6, {vertices_list[1], vertices_list[9], vertices_list[8]}, {edges_list[7], edges_list[8], edges_list[8]}},
-        {7, {vertices_list[1], vertices_list[8], vertices_list[3]}, {edges_list[8], edges_list[17], edges_list[19]}},
-        {8, {vertices_list[1], vertices_list[3], vertices_list[7]}, {edges_list[19], edges_list[9], edges_list[9]}},
-
-        {9, {vertices_list[2], vertices_list[6], vertices_list[10]}, {edges_list[13], edges_list[27], edges_list[11]}},
-        {10, {vertices_list[2], vertices_list[3], vertices_list[6]}, {edges_list[12], edges_list[16], edges_list[13]}},
-        {11, {vertices_list[2], vertices_list[7], vertices_list[3]}, {edges_list[13], edges_list[3], edges_list[12]}},
-
-        {12, {vertices_list[3], vertices_list[8], vertices_list[9]}, {edges_list[17], edges_list[18], edges_list[18]}},
-        {13, {vertices_list[3], vertices_list[9], vertices_list[5]}, {edges_list[18], edges_list[26], edges_list[6]}},
-
-        {14, {vertices_list[4], vertices_list[10], vertices_list[11]}, {edges_list[20], edges_list[0], edges_list[21]}},
-        {15, {vertices_list[4], vertices_list[11], vertices_list[5]}, {edges_list[21], edges_list[25], edges_list[22]}},
-        {16, {vertices_list[4], vertices_list[5], vertices_list[9]}, {edges_list[22], edges_list[26], edges_list[26]}},
-        {17, {vertices_list[4], vertices_list[9], vertices_list[3]}, {edges_list[26], edges_list[18], edges_list[28]}},
-        {18, {vertices_list[4], vertices_list[3], vertices_list[6]}, {edges_list[28], edges_list[16], edges_list[23]}},
-        {19, {vertices_list[4], vertices_list[6], vertices_list[10]}, {edges_list[23], edges_list[27], edges_list[20]}}
+    vector<Edge> edges_list = {
+        {0, 0, 5, false}, {1, 0, 1, false}, {2, 0, 11, false}, {3, 0, 10, false}, {4, 0, 7, false},
+        {5, 1, 5, false}, {6, 1, 7, false}, {7, 1, 8, false}, {8, 1, 9, false}, {9, 5, 9, false},
+        {10, 5, 11, false}, {11, 9, 8, false}, {12, 8, 7, false}, {13, 9, 4, false}, {14, 9, 3, false},
+        {15, 4, 3, false}, {16, 5, 4, false}, {17, 4, 11, false}, {18, 2, 4, false}, {19, 2, 11, false},
+        {20, 2, 3, false}, {21, 2, 6, false}, {22, 6, 3, false}, {23, 8, 3, false}, {24, 8, 6, false},
+        {25, 7, 6, false}, {26, 2, 10, false}, {27, 10, 6, false}, {28, 10, 7, false}, {29, 11, 10, false}
     };
+
+	vector<Face> faces_list = {
+		{0, {vertices_list[5], vertices_list[0], vertices_list[11]}, {edges_list[0], edges_list[2], edges_list[10]}},
+		{1, {vertices_list[0], vertices_list[5], vertices_list[1]}, {edges_list[0], edges_list[5], edges_list[1]}},
+		{2, {vertices_list[5], vertices_list[1], vertices_list[9]}, {edges_list[5], edges_list[8], edges_list[9]}},
+		{3, {vertices_list[5], vertices_list[4], vertices_list[9]}, {edges_list[16], edges_list[13], edges_list[9]}},
+		{4, {vertices_list[4], vertices_list[5], vertices_list[11]}, {edges_list[16], edges_list[10], edges_list[17]}},
+		{5, {vertices_list[4], vertices_list[3], vertices_list[9]}, {edges_list[15], edges_list[14], edges_list[13]}},
+		{6, {vertices_list[4], vertices_list[11], vertices_list[2]}, {edges_list[17], edges_list[19], edges_list[18]}},
+		{7, {vertices_list[2], vertices_list[3], vertices_list[6]}, {edges_list[20], edges_list[22], edges_list[21]}},
+		{8, {vertices_list[6], vertices_list[2], vertices_list[10]}, {edges_list[21], edges_list[26], edges_list[27]}},
+		{9, {vertices_list[3], vertices_list[8], vertices_list[6]}, {edges_list[23], edges_list[24], edges_list[22]}},
+		{10, {vertices_list[8], vertices_list[9], vertices_list[3]}, {edges_list[11], edges_list[14], edges_list[23]}},
+		{11, {vertices_list[9], vertices_list[8], vertices_list[1]}, {edges_list[11], edges_list[7], edges_list[8]}},
+		{12, {vertices_list[8], vertices_list[7], vertices_list[1]}, {edges_list[12], edges_list[6], edges_list[7]}},
+		{13, {vertices_list[6], vertices_list[7], vertices_list[8]}, {edges_list[25], edges_list[12], edges_list[24]}},
+		{14, {vertices_list[7], vertices_list[10], vertices_list[6]}, {edges_list[28], edges_list[27], edges_list[25]}},
+		{15, {vertices_list[7], vertices_list[10], vertices_list[0]}, {edges_list[28], edges_list[3], edges_list[4]}},
+		{16, {vertices_list[0], vertices_list[10], vertices_list[11]}, {edges_list[3], edges_list[29], edges_list[2]}},
+		{17, {vertices_list[4], vertices_list[2], vertices_list[3]}, {edges_list[18], edges_list[20], edges_list[15]}},
+		{18, {vertices_list[1], vertices_list[0], vertices_list[7]}, {edges_list[1], edges_list[4], edges_list[6]}},
+		{19, {vertices_list[11], vertices_list[2], vertices_list[10]}, {edges_list[19], edges_list[26], edges_list[29]}}
+	};
 
     for(size_t j = 0; j < faces_list.size() - 1; j++)
     {
@@ -140,8 +130,8 @@ TEST(SortTest, SortEdgesTest)
 
         for (size_t i = 0; i < sorted.size() - 1; ++i)
         {
-            EXPECT_EQ(sorted[i].end, sorted[(i + 1) % E].origin);
-            cerr << "Error between edge " << i << " and " << i + 1 << " of face " << j << endl;
+            EXPECT_EQ(sorted[i].end, sorted[(i + 1) % E].origin)
+             << "Error between edge " << i << " and " << (i + 1) % E << " of face " << j << endl;
         }
     }
 }
